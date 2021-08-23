8c53eecdf7455e093147e4590a14022ffc4bd30b
dimashadii <68457260+dimashadii@users.noreply.github.com>
2021-08-23 20:08:32 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('admin', function () {
+    return view('admin');
+})->middleware('checkRole:admin');
+Route::get('penjual', function () {
+    return view('penjual');
+})->middleware(['checkRole:penjual,admin']);
+Route::get('pembeli', function () {
+    return view('pembeli');
+})->middleware(['checkRole:pembeli,admin']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+use App\Http\Controllers\PostController;
+
+Route::resource('posts', PostController::class);
---
+++ storage/framework/views/aaf8a7e3fd5124b949d0ae2bdfeada411e31f16d.php
@@ -0,0 +1,42 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+            
+
+<a class="navbar-brand" href="<?php echo e(url('/')); ?>">
+    <?php echo e(config('app.name', 'Laravel')); ?>
+
+</a>
+<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="<?php echo e(url('admin')); ?>">Halaman Admin</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?php echo e(url('penjual')); ?>">Halaman Hubin</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?php echo e(url('pembeli')); ?>">Halaman Siswa</a>
+    </li>
+</ul>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><?php echo e(__('Dashboard')); ?></div>
+
+                <div class="card-body">
+                    <?php if(session('status')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(session('status')); ?>
+
+                        </div>
+                    <?php endif; ?>
+
+                    <?php echo e(__('You are logged in!')); ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\latihan_middleware\resources\views/home.blade.php ENDPATH**/ ?>
